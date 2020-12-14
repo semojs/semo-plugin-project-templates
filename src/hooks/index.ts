97cc1a1e34622849c65cd46da84ad23b3af20029
Richard Yu <vipzhicheng@gmail.com>
2020-12-14 20:28:06 +0800

    fix: add a template: semo-starter-frontend-typescript

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,17 +3,23 @@ import { UtilsType } from '@semo/core'
 export default (Utils: UtilsType) => {
   return {
     hook_create_project_template: new Utils.Hook('semo', {
-      semo_application_backend_starter: {
-        name: 'Semo application backend starter',
+      'semo-starter-backend-typescript': {
+        name: 'Semo inside application backend starter',
         description: 'Semo inside backend starter in typescript.',
         tags: ['backend'],
         repo: 'https://github.com/semojs/semo-starter-backend-typescript'
       },
-      semo_simple_backend_typescript_starter: {
-        name: 'Semo simple backend typescript project',
-        description: 'A tiny simple Semo project with typescript.',
+      'semo-starter-application-simple-typescript': {
+        name: 'Simple backend typescript starter',
+        description: 'A tiny simple Semo project with typescript and Semo.',
         tags: ['backend'],
         repo: 'https://github.com/semojs/semo-starter-application-simple-typescript.git'
+      },
+      'semo-starter-frontend-typescript': {
+        name: 'Simple frontend typescript starter',
+        description: 'A simple webpack typescript starter for frontend developers.',
+        tags: ['frontend'],
+        repo: 'https://github.com/semojs/semo-starter-frontend-typescript.git'
       },
     })
   }
